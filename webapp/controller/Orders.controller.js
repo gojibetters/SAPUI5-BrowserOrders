83fc2df8f556d4d6a4159b83148e39c0f94aa329
Bruno Patez <patezbr@gmail.com>
2023-11-08 16:27:16 -0300

    feat: add SearchField to Orders view

--- webapp/controller/Orders.controller.js
+++ webapp/controller/Orders.controller.js
@@ -1,13 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
     "com/lab2dev/browserorders/model/models",
     "../model/formatter"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel, models, formatter) {
+    function (Controller, JSONModel, Filter, FilterOperator, models, formatter) {
         "use strict";
 
         return Controller.extend("com.lab2dev.browserorders.controller.Orders", {
@@ -29,6 +31,22 @@ sap.ui.define([
                         }
                     })
                 })
+            },
+
+            onSearch: function (oEvent) {
+                let aFilters = []
+                let sQuery = oEvent.getSource().getValue()
+
+                if (sQuery && sQuery.length > 0) {
+                    let filter = new Filter("ShipName", FilterOperator.Contains, sQuery)
+
+                    aFilters.push(filter)
+                }
+
+                const oList = this.byId("ordersList")
+                const oBinding = oList.getBinding("items")
+                oBinding.filter(aFilters)
+
             }
         });
     });
